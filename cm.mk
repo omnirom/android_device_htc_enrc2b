9c28e0a233b6dab5afd6c7cb39cfc76aeb73bac3
Rob Shannon <spirit2457@gmail.com>
2013-05-23 11:19:07 +0200

    duplicate

--- cm.mk
+++ cm.mk
@@ -13,7 +13,6 @@ TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 #Set build fingerprint / ID / Product Name etc.
