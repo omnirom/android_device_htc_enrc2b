2455fda4e08908e1f662091a01f12997241f9ccc
Rob Shannon <lloir@kanger.me>
2013-08-02 13:17:06 +0400

    disable se for now
    Change-Id: I5a005f7a389100929a80c4a2f485005bb67ea51f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,7 +68,7 @@ TARGET_KERNEL_CONFIG := blade_cm10_oc_cpuquiet_defconfig
 # dont build docs
 DISABLE_DROIDDOC := true
 
-HAVE_SELINUX := true
+HAVE_SELINUX := false
 
 ifeq ($(HAVE_SELINUX),true)
 
