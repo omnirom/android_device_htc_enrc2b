379d7109b18c37ce4fb33a96be113902c4c62e1a
Rob Shannon <lloir@kanger.me>
2013-08-08 18:04:36 +0400

    Updates for cm-10.2 this is a squash of a load of commits from github/nightlies/
      4.2.2 blobage upgrade
      remove usb otg from storage list
      re-add BOARD_USES_GENERIC_AUDIO := false
      remove usb audio
      Enable support for mHeap-based screenshots
      unify fstab and enable fstabv2
      removing the need for out init.rc
      removing our init.rc
      moving pollyd to tegra3-commons
      Removed Pollyd > T3-commons
    
    Change-Id: I9eb6bcf935cc213775007416c0df7bc1a640ffec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,8 @@ WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcm4334.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcm4334_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/firmware/fw_bcm4334_p2p.bin"
 
-# EGL settings
-USE_OPENGL_RENDERER := true
-BOARD_EGL_NEEDS_LEGACY_FB := true
+# Camera
+COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
 
 # BT
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/enrc2b/bluetooth
@@ -44,6 +43,9 @@ BOARD_BLUEDROID_VENDOR_CONF := device/htc/enrc2b/bluetooth/vnd_enrc2b.txt
 
 # HTC ril compatability
 TARGET_PROVIDES_LIBRIL := device/htc/enrc2b/proprietary/lib/libhtc-ril.so
+
+#Audio
+BOARD_USES_GENERIC_AUDIO := false
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun0/file"
@@ -54,10 +56,10 @@ BOARD_HAS_SDCARD_INTERNAL := true
 # Kernel / Ramdisk
 TARGET_PROVIDES_INIT_TARGET_RC := true
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2302672896 
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Boot/Recovery image settings
@@ -72,16 +74,18 @@ TARGET_KERNEL_CONFIG := blade_cm10_oc_cpuquiet_defconfig
 
 # dont build docs
 DISABLE_DROIDDOC := true
-TARGET_PREBUILT_KERNEL := device/htc/enrc2b/recovery/recovery-kernel
+#TARGET_PREBUILT_KERNEL := device/htc/enrc2b/recovery/recovery-kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/htc/enrc2b/ramdisk/fstab.enrc2b
+RECOVERY_FSTAB_VERSION := 2 
 
 # SE Linux policies
-BOARD_SEPOLICY_DIRS := \
-    device/htc/enrc2b/selinux
+#BOARD_SEPOLICY_DIRS := \
+#    device/htc/enrc2b/selinux
 
-BOARD_SEPOLICY_UNION := \
-    file_contexts \
-    file.te \
-    device.te \
-    domain.te
+#BOARD_SEPOLICY_UNION := \
+#    file_contexts \
+#    file.te \
+#    device.te \
+#    domain.te
--- cm_enrc2b.mk
+++ cm_enrc2b.mk
@@ -38,7 +38,6 @@ PRODUCT_COPY_FILES += \
 
 # Files needed for boot image
 PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/ramdisk/init.rc:root/init.rc \
     $(LOCAL_PATH)/ramdisk/init.enrc2b.rc:root/init.enrc2b.rc \
     $(LOCAL_PATH)/ramdisk/init.usb.rc:root/init.usb.rc \
     $(LOCAL_PATH)/ramdisk/init.trace.rc:root/init.trace.rc \
@@ -186,18 +185,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/tfa/voice.preset:system/etc/tfa/voice.preset \
     $(LOCAL_PATH)/prebuilt/tfa/voice.speaker:system/etc/tfa/voice.speaker
 
-# Nexus 7 graphics blobs needed for 4.3+
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/prebuilt/lib/hw/gralloc.default.so:system/lib/hw/gralloc.default.so \
-$(LOCAL_PATH)/prebuilt/lib/hw/gralloc.tegra3.so:system/lib/hw/gralloc.tegra3.so \
-$(LOCAL_PATH)/prebuilt/lib/hw/hwcomposer.tegra3.so:system/lib/hw/hwcomposer.tegra3.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvapputil.so:system/lib/libnvapputil.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvasfparserhal.so:system/lib/libnvasfparserhal.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvaviparserhal.so:system/lib/libnvaviparserhal.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvavp.so:system/lib/libnvavp.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvddk_2d.so:system/lib/libnvddk_2d.so \
-$(LOCAL_PATH)/prebuilt/lib/libnvsm.so:system/lib/libnvsm.so
-
 # Hostapd
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/hostap/hostapd:system/bin/hostapd
@@ -223,15 +210,9 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    audio.usb.default \
     libtinyalsa \
     libaudioutils \
     libinvensense_mpl
-
-# pollyd
-PRODUCT_PACKAGES += \
-    Polly \
-    pollyd
 
 # other apps
 PRODUCT_PACKAGES += \
