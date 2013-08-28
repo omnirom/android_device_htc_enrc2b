67b790e45437ed0e903d0050ef988afbe0f42bc8
Thomas Wendt <thoemy@gmx.net>
2013-08-29 00:29:39 +0200

    enrc2b: Device repo cleanup part 1
    Needs the accompanied tegra3-common and vendor changes. Changes should not
    change the ROM because files were just moved around.
    
    Squashed commit of the following:
    
    commit 99ada0da3b0f807a7dfe78780d52957c14cd99bb
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 22:55:05 2013 +0200
    
        Remove common asound.conf
    
        Change-Id: I89a48889ee140d8db7b899db9dab96a996a440d3
    
    commit 7f8613d0e608b6b1d69ea61374f1a9d5762f9807
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 21:46:13 2013 +0200
    
        Move input files to tegra3-common
    
        Change-Id: I5e0bf9aeb12e1ab80c5d9991b6b43180cef41c4e
    
    commit db8932fa0a06c4c85b5200c4e55cc87518a07c4e
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 21:37:39 2013 +0200
    
        Remove more prebuilts that are added by Android packages
    
        Change-Id: Ife071480c1e3abe65fb3f5156d3799f539ca9c6a
    
    commit a7d5ef980a7cf147f5464248b533e21c831ea9e3
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 21:26:05 2013 +0200
    
        Remove prebuilts that are added by Android packages
    
        Change-Id: I5e5678d7115689935c0d659d8faba9cc8cb81251
    
    commit 991d3a998a9c3ef9d013167aea61b4f36d3388bf
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 21:03:45 2013 +0200
    
        Move prebuilt/dsp files to vendor
    
        Except AIC3008_REG_DualMic_XC.csv which is not used by enrc2b.
    
        Change-Id: I5b4db2cb804c9f2685683f7b2c8c65486c6606b6
    
    commit 8c55423d5825ed59b9c51d96b9ecbdc1a386fe72
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 20:10:32 2013 +0200
    
        Remove prebuild audio libs, exact same files are already in the vendor dir
    
        Change-Id: I54ec5bbad1813594cfb1775f4bb1bff7e218da7c
    
    commit a3b0aa2bd72990914ec0111b7407803c0a8a4e02
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 20:04:04 2013 +0200
    
        Move sound tfa files to vendor
    
        Change-Id: Ia41d0e4eaa95cff4623516f375d3e0277a56f0ee
    
    commit a49c9960e2f13d7e8d2ce73f5b58cd2084c2f038
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 19:56:29 2013 +0200
    
        Move soundimage files to vendor
    
        Change-Id: Ie194ff7e14e7007437a19ab0cfbfc5691eb09554
    
    commit 9d17bcbaebab45e6bc1a208c582c7b2b85bf61fd
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 19:36:33 2013 +0200
    
        Remove common defines
    
        Change-Id: Iae55e6af2768e04deaf8bd849318db088177ccb9
    
    commit 69afe3b7c830488134c1283a67cd5c7389d82073
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 19:35:30 2013 +0200
    
        Remove common files
    
        Change-Id: Iae8e49c5a3d5cbe23a48aa76ea6943e8b2ce208d
    
    commit 840280185d38f9c0ee1d9311603281b841701b3a
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 00:34:21 2013 +0200
    
        Remove common product packages
    
        Change-Id: I3e9ab3704442d9e1374513392fbe8a680b337d66
    
    commit bccfdd9c95014393dcdc39bebf43c3ae3f2901b4
    Author: Thomas Wendt <thoemy@gmx.net>
    Date:   Wed Aug 28 00:23:41 2013 +0200
    
        Delete common alsa prebuilts
    
        Change-Id: I4a8742127373f369c6ac74427f2ce9598ce089a9
    
    Change-Id: Icd7198d0bb86aac23e4c7150ace1360e09212bad

--- enrc2b.mk
+++ enrc2b.mk
@@ -37,10 +37,6 @@ PRODUCT_COPY_FILES := \
 
 # Prebuilt Audio/GPS/Camera/Wi-Fi configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/dsp/AIC3008_REG_DualMic_XA.csv:system/etc/AIC3008_REG_DualMic_XA.csv \
-    $(LOCAL_PATH)/prebuilt/dsp/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
-    $(LOCAL_PATH)/prebuilt/dsp/DSP_number.txt:system/etc/DSP_number.txt \
-    $(LOCAL_PATH)/configs/nvcamera.conf:system/etc/nvcamera.conf \
     $(LOCAL_PATH)/configs/nvcamera_2nd.conf:system/etc/nvcamera_2nd.conf \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
@@ -53,129 +49,21 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
     $(LOCAL_PATH)/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     $(LOCAL_PATH)/configs/calibration:system/etc/calibration \
-    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/enctune.conf:system/etc/enctune.conf \
     $(LOCAL_PATH)/configs/sysctl.conf:system/etc/sysctl.conf \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
-
-  # Prebuilt Alsa configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
-    $(LOCAL_PATH)/prebuilt/usr/share/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
 
 # Input device configeration files
 # for remapped APP_SWITCH to MENU
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
 
-# properitary ones
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/usr/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl \
-    $(LOCAL_PATH)/prebuilt/usr/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl \
-    $(LOCAL_PATH)/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-    $(LOCAL_PATH)/prebuilt/usr/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc \
-    $(LOCAL_PATH)/prebuilt/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc
-
-# Sound
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/soundimage/CodecDSPID.txt:system/etc/soundimage/CodecDSPID.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/CodecDSPID_XA.txt:system/etc/soundimage/CodecDSPID_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_BT_CarMode.txt:system/etc/soundimage/Sound_BT_CarMode.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_FM_HP.txt:system/etc/soundimage/Sound_FM_HP.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_FM_HP_XA.txt:system/etc/soundimage/Sound_FM_HP_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_FM_SPK.txt:system/etc/soundimage/Sound_FM_SPK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_FM_SPK_XA.txt:system/etc/soundimage/Sound_FM_SPK_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Note_Recording.txt:system/etc/soundimage/Sound_Note_Recording.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_DOCK.txt:system/etc/soundimage/Sound_Original_DOCK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_HP.txt:system/etc/soundimage/Sound_Original_HP.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_HP_XA.txt:system/etc/soundimage/Sound_Original_HP_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_SPK_RING.txt:system/etc/soundimage/Sound_Original_SPK_RING.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_SPK_RING_XA.txt:system/etc/soundimage/Sound_Original_SPK_RING_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original_SPK_XA.txt:system/etc/soundimage/Sound_Original_SPK_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_BT.txt:system/etc/soundimage/Sound_Phone_Original_BT.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_DOCK.txt:system/etc/soundimage/Sound_Phone_Original_DOCK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_HAC.txt:system/etc/soundimage/Sound_Phone_Original_HAC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_HP.txt:system/etc/soundimage/Sound_Phone_Original_HP.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_SPK.txt:system/etc/soundimage/Sound_Phone_Original_SPK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_SPK_XA.txt:system/etc/soundimage/Sound_Phone_Original_SPK_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Phone_Original_TTY.txt:system/etc/soundimage/Sound_Phone_Original_TTY.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Rec_Landscape.txt:system/etc/soundimage/Sound_Rec_Landscape.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Rec_Portrait.txt:system/etc/soundimage/Sound_Rec_Portrait.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_SpeakerVR_Recording.txt:system/etc/soundimage/Sound_SpeakerVR_Recording.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voice_Recording_AMR.txt:system/etc/soundimage/Sound_Voice_Recording_AMR.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_BT.txt:system/etc/soundimage/Sound_Voip_Original_BT.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_BT_AEC.txt:system/etc/soundimage/Sound_Voip_Original_BT_AEC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_HP.txt:system/etc/soundimage/Sound_Voip_Original_HP.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_HP_AEC.txt:system/etc/soundimage/Sound_Voip_Original_HP_AEC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_REC.txt:system/etc/soundimage/Sound_Voip_Original_REC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_REC_AEC.txt:system/etc/soundimage/Sound_Voip_Original_REC_AEC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_SPK.txt:system/etc/soundimage/Sound_Voip_Original_SPK.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_SPK_XA.txt:system/etc/soundimage/Sound_Voip_Original_SPK_XA.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_SPK_AEC.txt:system/etc/soundimage/Sound_Voip_Original_SPK_AEC.txt \
-    $(LOCAL_PATH)/prebuilt/soundimage/Sound_Voip_Original_SPK_AEC_XA.txt:system/etc/soundimage/Sound_Voip_Original_SPK_AEC_XA.txt
-
-# audio tfa - actually useless but keep it 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/tfa/fm.config:system/etc/tfa/fm.config \
-    $(LOCAL_PATH)/prebuilt/tfa/fm.eq:system/etc/tfa/fm.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/fm.preset:system/etc/tfa/fm.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/fm.speaker:system/etc/tfa/fm.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/playback.config:system/etc/tfa/playback.config \
-    $(LOCAL_PATH)/prebuilt/tfa/playback.eq:system/etc/tfa/playback.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/playback.preset:system/etc/tfa/playback.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/playback.speaker:system/etc/tfa/playback.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/recorder.config:system/etc/tfa/recorder.config \
-    $(LOCAL_PATH)/prebuilt/tfa/recorder.eq:system/etc/tfa/recorder.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/recorder.preset:system/etc/tfa/recorder.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/recorder.speaker:system/etc/tfa/recorder.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/ReleaseNote.txt:system/etc/tfa/ReleaseNote.txt \
-    $(LOCAL_PATH)/prebuilt/tfa/ring.config:system/etc/tfa/ring.config \
-    $(LOCAL_PATH)/prebuilt/tfa/ring.eq:system/etc/tfa/ring.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/ring.preset:system/etc/tfa/ring.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/ring.speaker:system/etc/tfa/ring.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/tcoef.speaker:system/etc/tfa/tcoef.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/tfa9887.config:system/etc/tfa/tfa9887.config \
-    $(LOCAL_PATH)/prebuilt/tfa/tfa9887.patch:system/etc/tfa/tfa9887.patch \
-    $(LOCAL_PATH)/prebuilt/tfa/tfa9887.speaker:system/etc/tfa/tfa9887.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/video.config:system/etc/tfa/video.config \
-    $(LOCAL_PATH)/prebuilt/tfa/video.eq:system/etc/tfa/video.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/video.preset:system/etc/tfa/video.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/video.speaker:system/etc/tfa/video.speaker \
-    $(LOCAL_PATH)/prebuilt/tfa/voice.config:system/etc/tfa/voice.config \
-    $(LOCAL_PATH)/prebuilt/tfa/voice.eq:system/etc/tfa/voice.eq \
-    $(LOCAL_PATH)/prebuilt/tfa/voice.preset:system/etc/tfa/voice.preset \
-    $(LOCAL_PATH)/prebuilt/tfa/voice.speaker:system/etc/tfa/voice.speaker
-
 #Audio packages
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    libtinyalsa \
-    libaudioutils \
     libinvensense_mpl
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    make_ext4fs \
-    setup_fs \
     sdcard \
     libmtp
 
@@ -184,25 +72,9 @@ PRODUCT_PACKAGES += \
     hostapd_cli \
     calibrator
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers
-
 #NFC
 PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_ndef \
-    libnfc_jni \
-    Nfc \
-    Tag \
-    com.android.nfc_extras
-
-# Other apps
-PRODUCT_PACKAGES += \
-    Torch \
-    com.android.future.usb.accessory
+    libnfc_ndef
 
 # Power
 PRODUCT_PACKAGES += \
@@ -228,13 +100,10 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_LOCALES += en_GB xhdpi
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product-if-exists, vendor/htc/enrc2b/enrc2b-vendor.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # common tegra3-HOX+ configs
 $(call inherit-product, device/htc/tegra3-common/tegra3.mk)
