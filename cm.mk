2051b28791e07c69cbcc04e01d83b26a1c5e54fb
Tom Briden <tom@decompile.me.uk>
2013-09-06 12:17:54 +0100

    enrc2b: got the resolution width/height the wrong way round
    Change-Id: I638aef4208b81e7bc4188352040d4d2bf5b9c088

--- cm.mk
+++ cm.mk
@@ -5,8 +5,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 PRODUCT_RELEASE_NAME := enrc2b
 
 # Resolution values for bootanimation
-TARGET_SCREEN_HEIGHT := 720
-TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
