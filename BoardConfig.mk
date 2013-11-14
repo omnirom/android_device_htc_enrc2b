946c9afa078c2ad908b02ecd68a171c58f6ab509
Rob Shannon <spirit2457@gmail.com>
2013-11-14 11:46:42 +0000

    we now use oss ril
    Change-Id: Ic5dfe27d9fe9a366ab36c25b018220f232e523f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/enrc2b/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/htc/enrc2b/bluetooth/vnd_enrc2b.txt
 
-# HTC ril compatability
-TARGET_PROVIDES_LIBRIL := device/htc/enrc2b/proprietary/lib/libhtc-ril.so
-
 # Kernel / Ramdisk
 TARGET_PROVIDES_INIT_TARGET_RC := true
 TARGET_USERIMAGES_USE_EXT4 := true
